6c4e13d79afe1bf9bcc632021aa0a4bbdf91781a
denisbereznuuk <denisbereznuuk@gmail.com>
2022-03-01 21:42:09 +0200

    Spring-Boot Framework initialization and starting done

--- DB.sql
+++ DB.sql
@@ -13,3 +13,16 @@ humidity INT CHECK(humidity >= 0 AND humidity <= 100) DEFAULT 0,
 atmo_pressure INT NOT NULL,
 min_cloud INT NOT NULL
 );
+/*
+SELECT * FROM MeteoData;
+
+LOAD DATA LOCAL INFILE "C:\Users\1\Desktop\2012-1.csv" INTO TABLE Meteostation.MeteoData
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(day_num, times, temp, dir, speed, weather, cloud, vrange, hum, atmo, min)
+SET
+date_time = CAST(CONCAT(DATE_FORMAT(day_num, '2012-1-%d'), ' ', times) AS DATETIME);
+
+SET GLOBAL local_infile=1;
+*/
