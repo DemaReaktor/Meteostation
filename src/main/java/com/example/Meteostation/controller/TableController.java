d597c2f72b5c69cf2903cd8dc792768a18d15020
denisbereznuuk <denisbereznuuk@gmail.com>
2022-03-14 23:42:58 +0200

    Table template init

--- src/main/java/com/example/Meteostation/controller/TableController.java
+++ src/main/java/com/example/Meteostation/controller/TableController.java
@@ -5,10 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
-    @GetMapping("/")
-    public String landingPage(Model model){
-        model.addAttribute("title", "Insight Weather");
-        return "Landing";//"home";//null;
+public class TableController {
+    @GetMapping("/table")
+    public String table(Model model){
+        return "TableTemplate";
     }
 }
